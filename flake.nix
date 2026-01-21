9f06be3595ce557929dbdbc1056d3db6843dcb29
ForeverAnApple <kinbd8@gmail.com>
2026-01-20 17:28:12 -0800

    dev: add nix devshell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Bun/TypeScript development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.zsh}/bin/zsh
+            exec $SHELL
+          '';
+        };
+      }
+    );
+}
